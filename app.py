0ead8bf46fae2dc5b46407a2ceedac28b12576b5
CaoHoangKhai <128301137+CaoHoangKhai@users.noreply.github.com>
2025-11-10 18:20:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,40 @@
-from flask import Flask, render_template, request, flash
+import os
 import pickle
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# Load models
-models = {
+# Đảm bảo load model từ đúng thư mục
+base_path = os.path.dirname(__file__)
 
-    'Naive Bayes': pickle.load(open('naive_bayes_model.pkl', 'rb')),
-    'Decision Tree': pickle.load(open('decision_tree_model.pkl', 'rb')), 
-    'Bagging': pickle.load(open('bagging_model.pkl', 'rb')),
-    'Logistic': pickle.load(open('logistic_model.pkl', 'rb')),
-    'Random Forest': pickle.load(open('random_forest_model.pkl', 'rb')),
+models = {
+    'Naive Bayes': pickle.load(open(os.path.join(base_path, 'naive_bayes_model.pkl'), 'rb')),
+    'Decision Tree': pickle.load(open(os.path.join(base_path, 'decision_tree_model.pkl'), 'rb')),
+    'Bagging': pickle.load(open(os.path.join(base_path, 'bagging_model.pkl'), 'rb')),
+    'Logistic': pickle.load(open(os.path.join(base_path, 'logistic_model.pkl'), 'rb')),
+    'Random Forest': pickle.load(open(os.path.join(base_path, 'Random_Forest_model.pkl'), 'rb')),
 }
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     prediction = None
     if request.method == 'POST':
         try:
-            # Retrieve form data
-            # area = float(request.form['area'])
             perimeter = float(request.form['perimeter'])
             major_axis_length = float(request.form['major_axis_length'])
             minor_axis_length = float(request.form['minor_axis_length'])
             eccentricity = float(request.form['eccentricity'])
             convex_area = float(request.form['convex_area'])
             extent = float(request.form['extent'])
-
-            # Get selected algorithm
             algorithm = request.form['algorithm']
 
-            # Prepare input data
-            input_data = [[ perimeter, major_axis_length, minor_axis_length, eccentricity, convex_area, extent]]
-
-            # Predict using the selected model
+            input_data = [[perimeter, major_axis_length, minor_axis_length,
+                           eccentricity, convex_area, extent]]
             prediction = models[algorithm].predict(input_data)[0]
-        except KeyError as e:
-            flash(f"Missing input field: {str(e)}. Please fill in all fields.")
-        except ValueError:
-            flash("Please enter valid numerical values for all fields.")
-
+        except Exception as e:
+            flash(str(e))
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
